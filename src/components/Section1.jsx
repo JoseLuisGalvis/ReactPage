1ee9a4f05b06d33c32131bf416007771e14a0240
joseLuisGalvis <jolugales@gmail.com>
2024-07-17 04:13:30 -0300

    Subiendo Proyecto React Vite

---
+++ src/components/Section1.jsx
@@ -0,0 +1,19 @@
+import 'react';
+
+const Section1 = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        backgroundColor: 'red',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Componente 1</h1>
+    </div>
+  );
+};
+
+export default Section1;
